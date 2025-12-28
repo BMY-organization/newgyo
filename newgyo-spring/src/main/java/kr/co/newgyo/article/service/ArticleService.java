6538bfb147f232563e6f84604efa35b8db561426
YUL554 <hyr0197@gmail.com>
2025-12-28 20:42:04 +0900

    refactor : 기사 저장 메소드 분리 및 트랜잭션 추가

--- newgyo-spring/src/main/java/kr/co/newgyo/article/service/ArticleService.java
+++ newgyo-spring/src/main/java/kr/co/newgyo/article/service/ArticleService.java
@@ -1,5 +1,6 @@
 package kr.co.newgyo.article.service;
 
+import jakarta.transaction.Transactional;
 import kr.co.newgyo.article.dto.ArticleListResponse;
 import kr.co.newgyo.article.dto.ArticleResponse;
 import kr.co.newgyo.article.entity.Article;
@@ -9,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -23,10 +25,10 @@ public class ArticleService {
     private final ArticleCrawlerService articleCrawlerService;
 
     @Scheduled(fixedDelay = 60 * 60 * 1000) // 1시간
-    public void scheduledCrawler(){
+    public void scheduledCrawler() {
         try {
             // 파이썬 서버 헬스 체크
-            if(!articleCrawlerService.isHealth()){
+            if (!articleCrawlerService.isHealth()) {
                 log.warn("[파이썬 서버 오류]");
                 return;
             }
@@ -42,29 +44,42 @@ public class ArticleService {
             List<ArticleResponse> response = listResponse.getArticleListResponse();
 
             log.info("[article 수] {}", listResponse.getCount());
-            response.forEach(article -> {log.info("[article] {}", article);});
+            response.forEach(article -> {
+                log.info("[article] {}", article);
+            });
 
-            // 크롤링 데이터 디비 저장
-            for(ArticleResponse data : response){
-                // 중복 기사 제외
-                if(repository.existsByUrl(data.getUrl())){
-                    return;
-                }
+            saveNewArticles(response);
 
-                Article article = Article.builder()
-                        .title(data.getTitle())
-                        .content(data.getContent())
-                        .language("Korea")
-                        .reporter(String.valueOf(data.getReporters()))
-                        .url(data.getUrl())
-                        .articleDate(data.getDate())
-                        .build();
-
-                repository.save(article);
-
-            }
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error("[크롤링 오류]", e);
+        }
+    }
+
+    @Transactional
+    public void saveNewArticles(List<ArticleResponse> response) {
+        List<Article> articles = new ArrayList<>();
+        // 크롤링 데이터 디비 저장
+        for (ArticleResponse data : response) {
+            // 중복 기사 제외
+            if (repository.existsByUrl(data.getUrl())) {
+                continue;
+            }
+
+            Article article = Article.builder()
+                    .title(data.getTitle())
+                    .content(data.getContent())
+                    .language("Korea")
+                    .reporter(String.valueOf(data.getReporters()))
+                    .url(data.getUrl())
+                    .articleDate(data.getDate())
+                    .build();
+
+            articles.add(article);
+        }
+
+        if(!articles.isEmpty()){
+            repository.saveAll(articles);
+            log.info("[뉴스 저장 완료] {} 건", articles.size());
         }
     }
 }
